83cb3441a338702903f87188df20881a97c63e37
peernisse <peernisse@gmail.com>
2019-10-16 19:16:12 -0600

    Setup sidebar menu tabs

--- wqdashboard/app.R
+++ wqdashboard/app.R
@@ -1,50 +1,34 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+## app.R ##
 library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+ui <- dashboardPage(
+  dashboardHeader(title='Water Quality Explore'),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("dashboard")),
+      menuItem("Data Import", tabName = "dataimport", icon = icon("th"))
+    )
+      
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "home",
+              h2('Water Quality Explorer'),
+              h3('Overview'),
+              h3('Disclaimer')
       ),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+      # Second tab content
+      tabItem(tabName = "dataimport",
+              h2("Data Import Instructions"),
+              h2('Required Data Format')
       )
-   )
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
+server <- function(input, output) { }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
