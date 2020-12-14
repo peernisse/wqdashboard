de04cb2eb7cdd396ecc29faef7382466461cf617
Peter EerNisse <peernisse@stantec.com>
2020-12-14 14:36:59 -0700

    Upping file upload size

--- wqdashboard/app.R
+++ wqdashboard/app.R
@@ -454,7 +454,7 @@ ui <- dashboardPage(
 
 server <- function(input, output,session) {
   
-  options(shiny.maxRequestSize=30*1024^2)
+  options(shiny.maxRequestSize=100*1024^2)
   
   
   #'####################################################
